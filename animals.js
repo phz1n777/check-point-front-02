a1b549a4c4fa7acc394b36152a7a90eed90754d9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:50:57 -0300

    checkpoint-2

---
+++ animals.js
@@ -0,0 +1,6 @@
+const animals = ["macaco", "tucano", "elefante", "pavão", "hipopótamo", "leão"]
+if(!animals.includes("leão")){
+    console.log("Leão não existe no array animals");
+} else{
+    console.log("Leão existe na array animals")
+}
---
+++ series.js
@@ -0,0 +1,4 @@
+my3FavoriteTvShows = ["Cidade de Deus", "Sintonia", "Última parada"]
+
+const i = my3FavoriteTvShows.length === 3;
+console.log(i);
